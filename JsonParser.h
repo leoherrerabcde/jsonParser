484820232a3f46155631913a4573901234138805
LeonardoHerrera <leo.herrera.bcde@gmail.com>
2019-01-30 17:04:52 -0300

    Adding Files

---
+++ JsonParser.h
@@ -0,0 +1,28 @@
+#ifndef JSONPARSER_H
+#define JSONPARSER_H
+
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/prettywriter.h" // for stringify JSON
+
+#include <cstdio>
+#include <iostream>
+#include <iostream>
+
+class JsonParser
+{
+    public:
+        JsonParser();
+        virtual ~JsonParser();
+
+        static int rapidJsontest1();
+        static int rapidJsontest2();
+
+    protected:
+
+    private:
+
+};
+
+#endif // JSONPARSER_H
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "JsonParser.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    JsonParser::rapidJsontest1();
+    JsonParser::rapidJsontest2();
+
+    return 0;
+}
